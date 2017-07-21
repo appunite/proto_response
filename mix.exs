47a0af413c12c797f2b607a661ef2894357714ab
Tobiasz <tobiasz.malecki@appunite.com>
2017-07-22 01:10:27 +0200

    version bump

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ProtoResponse.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
 
   def project do
     [
@@ -41,7 +41,7 @@ defmodule ProtoResponse.Mixfile do
   defp deps do
     [
       {:exprotobuf, "~> 1.0"},
-      {:phoenix,    "~> 1.1"}
+      {:phoenix,    "~> 1.1 or ~> 1.3-rc"}
     ]
   end
 end
