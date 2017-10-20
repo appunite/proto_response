681766e71d80b38acff138b3aea62ab5e24e6a9f
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2017-10-20 13:49:51 +0200

    Raise runtime error when module is not available

--- lib/proto_response.ex
+++ lib/proto_response.ex
@@ -28,14 +28,8 @@ defmodule ProtoResponse do
   def proto_response(conn, code, proto) do
     response = Phoenix.ConnTest.response(conn, code)
 
-    case Plug.Conn.get_resp_header(conn, "content-type") |> List.first do
-      "application/x-protobuf" <> _ ->
-        :ok
-      nil ->
-        raise RuntimeError, message: "no content-type was set, expected a protobuf response"
-      other ->
-        raise RuntimeError, message: "expected content-type for protobuf, got: \"#{other}\""
-    end
+    ensure_content_type_or_raise!(conn)
+    ensure_module_available_or_raise!(proto)
 
     try do
       %{__struct__: ^proto} = response |> proto.decode()
@@ -44,4 +38,26 @@ defmodule ProtoResponse do
         raise RuntimeError, message: "could not decode Protobuf body"
     end
   end
+
+  defp ensure_content_type_or_raise!(conn) do
+    case Plug.Conn.get_resp_header(conn, "content-type") |> List.first do
+      "application/x-protobuf" <> _ ->
+        :ok
+      nil ->
+        raise RuntimeError, message: "no content-type was set, expected a protobuf response"
+      other ->
+        raise RuntimeError, message: "expected content-type for protobuf, got: \"#{other}\""
+    end
+  end
+
+  defp ensure_module_available_or_raise!(proto_module) do
+    case Code.ensure_loaded(proto_module) do
+      {:module, ^proto_module} -> :ok
+      {:error, _} ->
+        raise RuntimeError, message: "module #{module_name(proto_module)} is not available"
+    end
+  end
+
+  defp module_name(module),
+    do: module |> to_string |> String.replace(~r/^Elixir\./, "")
 end
